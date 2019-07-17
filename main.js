9e7c982bb4e37320c93cfc8a22ac8b89b0989f65
Emanuela <ebargelesi@gmail.com>
2019-07-17 10:14:53 +0200

    Aggiornato, così prende la classe responsive "active".

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 //quando clicco sull'icona appare il menà
   $( ".fa-bars" ).click(function() {
 
-    $( ".hamburger-menu" ).show();
+    $( ".hamburger-menu" ).addClass( "active" );
   });
 
 
 //quando clicco sull'icona X il menù scompare
   $( ".fa-times" ).click(function() {
 
-    $( ".hamburger-menu" ).hide();
+    $( ".hamburger-menu" ).removeClass( "active" );
   });
 });
